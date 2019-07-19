bf2c7287af9857034e5b8a8c0ed099a2f5e149be
ZtcStart <1731468109@qq.com>
2019-07-19 18:45:05 +0800

    1.1

---
+++ week01/src/day5/SwitchTest.java
@@ -0,0 +1,19 @@
+package day5;
+
+/**
+ *
+ * 探究switch语句的可用数据类型
+ *
+ * 可用：byte、short、int、char、枚举
+ *
+ * double、float 、boolean 、long、数组不可用
+ */
+public class SwitchTest {
+
+    static void demo(DemoEnum i){
+        switch (i) {
+
+        }
+    }
+}
+
