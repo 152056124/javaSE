1054bc3337a0437f5a74f389b9079d8cf5616067
ZtcStart <1731468109@qq.com>
2019-07-19 13:52:04 +0800

    第一次上传

--- week01/src/day3/TwoDimensionalArray.java
+++ week01/src/day3/TwoDimensionalArray.java
@@ -9,7 +9,7 @@ public class TwoDimensionalArray {
         int[][] a = new int[3][4];
         a[0] = new int[2];
         a[1] = new int[5];
-        //a[2] = {2,3};
+        a[2] = new int[]{2,3};
         System.out.println(a[0][1]);
     }
     public static void main(String[] args) {
