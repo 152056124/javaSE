9529a18c9cdfac2ea1e52782d2eea645fc22dc3f
ZtcStart <1731468109@qq.com>
2019-07-20 09:40:46 +0800

    1.1

--- week01/src/day6/SourceCode.java
+++ week01/src/day6/SourceCode.java
@@ -2,12 +2,13 @@ package day6;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /**
  *
  * Java源码阅读
  */
-public class SourceCode {
+public class SourceCode extends Random{
     public static void main(String[] args) {
         //Object类
         Object object = new Object();
@@ -21,6 +22,23 @@ public class SourceCode {
         int[] array = new int[5];
         Arrays.sort(array);
         //Random类
+        /*
+            nextInt()本质是返回next(32);
+            nextInt(int bound)
+         */
         Random random = new Random();
+        int v1 = random.nextInt(10);
+        int v2 = (int)(Math.random() * 10);
+
+        //Math.random()本质上就是random.nextDouble();
+        double d1 = random.nextDouble();
+        double d2 = Math.random();
+
+        System.out.println(d1);
+        System.out.println(d2);
+        //System.out.println(v1);
+        //System.out.println(v2);
+        IntStream intStream = random.ints();
+
     }
 }
