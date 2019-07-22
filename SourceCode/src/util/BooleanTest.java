fd60ef580af897e41fc2246ba2854f4e0af2bd06
ZtcStart <1731468109@qq.com>
2019-07-22 18:32:34 +0800

    2.1

---
+++ SourceCode/src/util/BooleanTest.java
@@ -0,0 +1,9 @@
+package util;
+
+public class BooleanTest {
+    public static void main(String[] args) {
+        Boolean value1 = Boolean.valueOf(true);
+        Boolean value2 = false;
+        Boolean value3 = Boolean.valueOf("true");
+    }
+}
--- SourceCode/src/util/ObjectTest.java
+++ SourceCode/src/util/ObjectTest.java
@@ -1,4 +1,11 @@
 package util;
 
 public class ObjectTest {
+    public static void main(String[] args) {
+        Object o = new Object();
+        char[] a = {'a','1',99,101};
+        System.out.println(a);   //alce
+        int[] b = {1,2,3,4};
+        System.out.println(b);   //[I@1b6d3586
+    }
 }
