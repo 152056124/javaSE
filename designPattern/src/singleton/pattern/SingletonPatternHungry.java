175a3447e1545c59ce4fcbb65540a23748a17b91
ZtcStart <1731468109@qq.com>
2019-07-22 18:31:30 +0800

    2.1

--- designPattern/src/singleton/pattern/SingletonPatternHungry.java
+++ designPattern/src/singleton/pattern/SingletonPatternHungry.java
@@ -5,4 +5,9 @@ package singleton.pattern;
  * 饿汉式单例模式
  */
 public class SingletonPatternHungry {
+    private static SingletonPatternHungry singletonPatternHungry = new SingletonPatternHungry();
+    private SingletonPatternHungry(){}
+    public static SingletonPatternHungry getInstance(){
+        return singletonPatternHungry;
+    }
 }
--- designPattern/src/singleton/pattern/SingletonPatternLazy.java
+++ designPattern/src/singleton/pattern/SingletonPatternLazy.java
@@ -3,8 +3,17 @@ package singleton.pattern;
 /**
  *
  * 懒汉式单例模式
- *  安全
+ *  安全  有synchronized关键字修饰
  *  不安全
  */
 public class SingletonPatternLazy {
+    private static SingletonPatternLazy singletonPatternLazy;
+    private SingletonPatternLazy(){}
+    public static synchronized SingletonPatternLazy getInstance(){
+        if(singletonPatternLazy == null){
+            singletonPatternLazy = new SingletonPatternLazy();
+        }
+        return singletonPatternLazy;
+    }
+
 }
