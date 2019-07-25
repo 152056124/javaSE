51b0c9ff3748639725d3fc921bee3566e665fe9b
ZtcStart <1731468109@qq.com>
2019-07-25 16:22:25 +0800

    2.4

--- SourceCode/src/util/PackagingClass.java
+++ SourceCode/src/util/PackagingClass.java
@@ -7,12 +7,24 @@ package util;
 public class PackagingClass {
     public static void main(String[] args) {
         Integer integer = new Integer(5);
+        Integer integer1 = new Integer(5);
+        System.out.println(integer == integer1);//false
+        byte a = 127;
+        Byte aByte = Byte.valueOf(a);
+        short b = 300;
+        Short.valueOf(b);
+        Long C =  Long.valueOf(12);
+        Character d = Character.valueOf(' '); //Character的缓存数组是下于127，因为char没有负值。
+        Float e = Float.valueOf(12);
+        Double f = Double.valueOf(12);          //Double和Float没有缓存数组。
+
         Integer a1 = Integer.valueOf(127);
         Integer a2 = Integer.valueOf(127);
         Integer a3 = Integer.valueOf(128);
         Integer a4 = Integer.valueOf(128);
         System.out.println(a1 == a2); // true
         System.out.println(a4 == a3); //false
+        //缓冲数组仅在调用valueOf()生成包装类对象时有效。（自动装箱即采用的valueOf()方法）
         //如果传入Integer的数处于-128到127之间，则存储于cache数组中，为同一个对象引用。
         // 如果超过，则为不同对象。
 
