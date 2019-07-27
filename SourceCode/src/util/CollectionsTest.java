32e5fda709f184ef35b6fd39296ad5cb23826f5d
ZtcStart <1731468109@qq.com>
2019-07-27 16:15:04 +0800

    2.6

--- SourceCode/src/util/CollectionsTest.java
+++ SourceCode/src/util/CollectionsTest.java
@@ -1,21 +1,27 @@
 package util;
 
 import java.util.*;
-import java.util.function.Consumer;
 
 public class CollectionsTest {
-    public static void main(String[] args) {
+    static void arrayListDemo(){
         ArrayList<Integer> arrayList = new ArrayList();
         boolean b = arrayList.addAll(ArrayListTest.temp());
         System.out.println(b);
         System.out.println(Arrays.toString(arrayList.toArray()));
         System.out.println(Collections.binarySearch(arrayList, 15));
         Class<ArrayListTest> eClass =  ArrayListTest.class;
-       // List list = Collections.checkedList(arrayList, eClass);
+        // List list = Collections.checkedList(arrayList, eClass);
         Iterator i = arrayList.iterator();
         while (i.hasNext()){
             System.out.print(i.next()+"\t");
         }
         arrayList.forEach(new Consumer1());
+    }
+    static void mapDemo(){
+        HashMap<Integer,String> h = new HashMap<>();
+    }
+    public static void main(String[] args) {
+        //arrayListDemo();
+        mapDemo();
     }
 }
